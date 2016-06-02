629a5c5f96e4171ae93e4dd7c180f1fa85e20c39
Kevin Tucker <kwtucker4@gmail.com>
2016-06-02 00:14:49 -0400

    added basic unit test for root endpoint

---
+++ src/routes/api.js
@@ -0,0 +1,103 @@
+module.exports = function(express) {
+  var router = express.Router();
+
+// -=-=-=-=- Get All Apps -=-=-=-=-
+// -=-=-=-=--=-=-=-=--=-=-=-=--=-=
+  router.get('/apps/', function (req , res){
+      res.json({
+          "apps": [
+              {
+                "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
+                "title": "Best router Ever",
+                "description": "A fast paced side scrolling shooter",
+                "artAssets": [
+                  { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
+                  { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
+                ],
+                "releaseDate": "2016-06-15T22:29:20.000Z",
+                "createdAt": "2016-05-15T22:29:20.000Z",
+                "updatedAt": "2016-05-15T22:29:20.000Z",
+                "user": {
+                  "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+                  "name": "Chapman"
+                }
+              },
+              {
+                "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
+                "title": "Best router Ever2",
+                "description": "A fast paced side scrolling shooter",
+                "artAssets": [
+                  { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
+                  { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
+                ],
+                "releaseDate": "2016-06-15T22:29:20.000Z",
+                "createdAt": "2016-05-15T22:29:20.000Z",
+                "updatedAt": "2016-05-15T22:29:20.000Z",
+                "user": {
+                  "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+                  "name": "Chapman"
+                }
+              },
+              {
+                "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
+                "title": "Best router Ever3",
+                "description": "A fast paced side scrolling shooter",
+                "artAssets": [
+                  { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
+                  { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
+                ],
+                "releaseDate": "2016-06-15T22:29:20.000Z",
+                "createdAt": "2016-05-15T22:29:20.000Z",
+                "updatedAt": "2016-05-15T22:29:20.000Z",
+                "user": {
+                  "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+                  "name": "Chapman"
+                }
+              }
+          ]
+      })
+  })
+
+// -=-=-=-=- Get All Users -=-=-=-=-
+// -=-=-=-=--=-=-=-=--=-=-=-=--=-=
+  router.get('/users', function (req , res){
+    res.json({
+      "user": [
+        {
+          "id": "1",
+          "name": "Chapman"
+        },
+        {
+          "id": "2",
+          "name": "Kevin"
+        },
+        {
+          "id": "3",
+          "name": "Daniel"
+        },
+        {
+          "id": "4",
+          "name": "Krysler"
+        }
+      ]
+    })
+  })
+
+
+// -=-=-=-=- Accept and id for user -=-=-=-=-
+// -=-=-=-=--=-=-=-=--=-=-=-=--=-=-=-=-=-=-=-
+  router.get('/users/:id', function (req , res){
+    res.send('user: ' + req.params.id);
+  })
+
+// -=-=-=-=- Accept and id for Apps -=-=-=-=-
+// -=-=-=-=--=-=-=-=--=-=-=-=--=-=-=-=-=-=-=-
+  router.get('/apps/:id', function (req , res){
+    res.send('app: ' + req.params.id);
+  })
+
+  return router
+}
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,122 +1,20 @@
 var express   = require('express'),
+    body_parser = require('body-parser'),
     app       = express();
 
+// When Url starts with api, got to the routes/api and grab routes.
+app.use("/api", require('./routes/api.js')(express));
 
+// Root response
 app.get('/', function (req, res) {
     res.json({"hello": "World"})
+})
+
+var port = 3000;
+
+var server = app.listen(port, function() {
+  console.log('app is running on port:', port);
 });
 
-app.get('/api/apps', function (req , res){
-    res.json({
-        "apps": [
-            {
-              "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
-              "title": "Best App Ever",
-              "description": "A fast paced side scrolling shooter",
-              "artAssets": [
-                { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-                { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-              ],
-              "releaseDate": "2016-06-15T22:29:20.000Z",
-              "createdAt": "2016-05-15T22:29:20.000Z",
-              "updatedAt": "2016-05-15T22:29:20.000Z",
-              "user": {
-                "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-                "name": "Chapman"
-              }
-            },
-            {
-              "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
-              "title": "Best App Ever2",
-              "description": "A fast paced side scrolling shooter",
-              "artAssets": [
-                { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-                { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-              ],
-              "releaseDate": "2016-06-15T22:29:20.000Z",
-              "createdAt": "2016-05-15T22:29:20.000Z",
-              "updatedAt": "2016-05-15T22:29:20.000Z",
-              "user": {
-                "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-                "name": "Chapman"
-              }
-            },
-            {
-              "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
-              "title": "Best App Ever3",
-              "description": "A fast paced side scrolling shooter",
-              "artAssets": [
-                { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-                { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-              ],
-              "releaseDate": "2016-06-15T22:29:20.000Z",
-              "createdAt": "2016-05-15T22:29:20.000Z",
-              "updatedAt": "2016-05-15T22:29:20.000Z",
-              "user": {
-                "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-                "name": "Chapman"
-              }
-            }
-        ]
-    })
-})
-
-
-app.get('/api/apps/1', function (req , res){
-  res.json({
-      "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
-      "title": "Best App Ever3",
-      "description": "A fast paced side scrolling shooter",
-      "artAssets": [
-        { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-        { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-      ],
-      "releaseDate": "2016-06-15T22:29:20.000Z",
-      "createdAt": "2016-05-15T22:29:20.000Z",
-      "updatedAt": "2016-05-15T22:29:20.000Z",
-      "user": {
-        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-        "name": "Chapman"
-      }
-    })
-})
-
-app.get('/api/users', function (req , res){
-  res.json({
-    "user": [
-      {
-        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-        "name": "Chapman"
-      },
-      {
-        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-        "name": "Kevin"
-      },
-      {
-        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-        "name": "Daniel"
-      },
-      {
-        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-        "name": "Krysler"
-      }
-    ]
-  })
-})
-
-app.get('/api/users/1', function (req , res){
-  res.json({
-    "user":
-      {
-        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
-        "name": "Chapman"
-      }
-  })
-})
-
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('app is running on port:', port);
-console.log('Gulp Is Awesome');
-
+// This will allow the test to require the server
+module.exports = server;
