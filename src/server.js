f7073202598c5fd6447742ddd28b739d6076b72e
Kevin Tucker <kwtucker4@gmail.com>
2016-06-01 22:15:36 -0400

    Added 4 endpoints for All users, One user, All apps, One app.

--- src/server.js
+++ src/server.js
@@ -3,11 +3,10 @@ var express   = require('express'),
 
 
 app.get('/', function (req, res) {
-    res.render('home');
     res.json({"hello": "World"})
 });
 
-app.get('/api/all', function (req , res){
+app.get('/api/apps', function (req , res){
     res.json({
         "apps": [
             {
@@ -62,7 +61,62 @@ app.get('/api/all', function (req , res){
     })
 })
 
+
+app.get('/api/apps/1', function (req , res){
+  res.json({
+      "id": "0032c47b-4a7b-4232-9cc3-6af718244ea8",
+      "title": "Best App Ever3",
+      "description": "A fast paced side scrolling shooter",
+      "artAssets": [
+        { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
+        { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
+      ],
+      "releaseDate": "2016-06-15T22:29:20.000Z",
+      "createdAt": "2016-05-15T22:29:20.000Z",
+      "updatedAt": "2016-05-15T22:29:20.000Z",
+      "user": {
+        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+        "name": "Chapman"
+      }
+    })
+})
+
+app.get('/api/users', function (req , res){
+  res.json({
+    "user": [
+      {
+        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+        "name": "Chapman"
+      },
+      {
+        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+        "name": "Kevin"
+      },
+      {
+        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+        "name": "Daniel"
+      },
+      {
+        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+        "name": "Krysler"
+      }
+    ]
+  })
+})
+
+app.get('/api/users/1', function (req , res){
+  res.json({
+    "user":
+      {
+        "id": "ae25e5a4-73db-4969-9f6c-acf8246b7faa",
+        "name": "Chapman"
+      }
+  })
+})
+
+
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('app is running on port:', port);
 console.log('Gulp Is Awesome');
+
